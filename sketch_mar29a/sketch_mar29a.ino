d0dae9e8162c95734d42b7df7e82a1ef50123048
developer-sujon <muhammad.sujon.cse@gmail.com>
2026-03-30 01:11:29 +0600

    Circuit Control with Multiple GPIOs

--- sketch_mar29a/sketch_mar29a.ino
+++ sketch_mar29a/sketch_mar29a.ino
@@ -1,13 +1,33 @@
 void setup() {
-  // put your setup code here, to run once:
   pinMode(7, OUTPUT);
-
+  pinMode(8, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  digitalWrite(7,  HIGH);
-  delay(500);
-  digitalWrite(7,  LOW);
-  delay(500);
+
+  digitalWrite(7, HIGH);
+  delay(150);
+  digitalWrite(7, LOW);
+
+  digitalWrite(8, HIGH);
+  delay(150);
+  digitalWrite(8, LOW);
+
+  digitalWrite(9, HIGH);
+  delay(150);
+  digitalWrite(9, LOW);
+
+  digitalWrite(10, HIGH);
+  delay(150);
+  digitalWrite(10, LOW);
+
+  digitalWrite(9, HIGH);
+  delay(150);
+  digitalWrite(9, LOW);
+
+  digitalWrite(8, HIGH);
+  delay(150);
+  digitalWrite(8, LOW);
 }
